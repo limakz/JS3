7e3ca2d3d5f97b94a0e81cdf3592ddccda38d455
Kamil Przebieracz <limakz@o2.pl>
2019-08-22 13:03:01 +0200

    Finish JS3

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+var kot = "miau";
+var pies = "hau";
+var joinedStrings = ""
+joinedStrings = kot + pies + joinedStrings;
 
+console.log(joinedStrings);
+
+var myNumber = 4;
+var myString = "4";
+myNumber == myString;
+console.log(myNumber == myString);
+myNumber === myString;
+console.log(myNumber === myString);
+
+
+var counter = 145;
+counter = counter -1;
+console.log(counter);
+
+
+var liczba = 15;
+var liczba2 = 10;
+var result = null;
+result = liczba > liczba2;
+console.log(result);
